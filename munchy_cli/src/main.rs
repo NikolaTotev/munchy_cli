32f68172ffcb7153817ff958cb4ff7c45207def0
NikolaTotev <nikola.totev@ocado.com>
2023-07-07 11:52:10 +0300

    feat: Add roadmap

---
+++ munchy_cli/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    println!("Hello, world!");
+}
+//Step 1
+/*
+
+* Model a recipe, food ingredient, cook book and ingredient database.
+* Implement creation/instantiation for each of the items mentioned above.
+* Implement adding a food ingredient by name and by calories/100g
+* Implement creating a recipe by adding a name, food ingredient with quantities. 
+  Validate the ingredient is actually in the igredient db.
+* Implement adding a recipe to the cookbook
+* Implement listing of ingredients in the database and cookbook  
+* Implement viewing recipe by name
+
+*/
+
+//Step 2
+/*
+
+* Implement basic search by name
+* Implement search that takes in an ingredient and returnc recipes that have at least one of the ingredients 
+* Implement saving to json for the db of ingredients and the cookbook
+* Implement opnening json for ingredient db and cookbook
+
+*/
+
+//Step 3
+/*
+
+* Improve printing to console.
+* Improve user interface
+
+*/
